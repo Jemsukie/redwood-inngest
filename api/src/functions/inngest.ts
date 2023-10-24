58fbcd503723ebdee43bb49d32284f18102442d5
Jemuel Lupo <jemuel.lupo@gmail.com>
2023-10-24 22:08:45 +0800

    feat(): use web api url for deployment

--- api/src/functions/inngest.ts
+++ api/src/functions/inngest.ts
@@ -4,6 +4,6 @@ import fns from 'src/inngest'
 import { inngest } from 'src/inngest/client'
 
 export const handler = serve(inngest, fns, {
-  servePath: '/.redwood/functions/inngest',
+  servePath: `${process.env.WEB_API_URL || '/.redwood/functions'}/inngest`,
   signingKey: process.env.SIGNING_KEY,
 })
