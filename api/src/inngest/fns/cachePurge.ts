ffcaf0e73589e6f96baa9af7eacd73123db2775a
Jemuel Lupo <jemuel.lupo@gmail.com>
2023-10-26 19:14:58 +0800

    test(): use axios for fetching api

--- api/src/inngest/fns/cachePurge.ts
+++ api/src/inngest/fns/cachePurge.ts
@@ -4,7 +4,7 @@ import { inngest } from '../client'
 
 export default inngest.createScheduledFunction(
   'Purge cache every morning',
-  '* * * * *',
+  '0 0 * * *',
   async () => {
     // Purge the cache
     await new Promise((resolve) => setTimeout(resolve, Math.random() * 1000))
--- api/src/lib/order.ts
+++ api/src/lib/order.ts
@@ -1,24 +1,35 @@
+import axios from 'axios'
+
 export const order = async () => {
-  await fetch('https://api.brevo.com/v3/smtp/email', {
-    // TODO, merge options.headers if exist
-    headers: {
-      accept: 'application/json',
-      'api-key': process.env.BREVO_API_KEY || '',
-    },
-    method: 'POST',
-    body: JSON.stringify({
-      sender: {
-        name: `"Atlas Admin" <ops@thefreelancemovement.com>`,
-        email: 'jemuel.lupo@gmail.com',
-      },
-      to: [
-        {
+  try {
+    const response = await axios.post(
+      'https://api.brevo.com/v3/smtp/email',
+      {
+        sender: {
+          name: `"Atlas Admin" <ops@thefreelancemovement.com>`,
           email: 'jemuel.lupo@gmail.com',
-          name: 'Dev Testing',
         },
-      ],
-      htmlContent: 'Hello this is testing',
-      subject: 'Testing this part',
-    }),
-  })
+        to: [
+          {
+            email: 'jemuel.lupo@gmail.com',
+            name: 'Dev Testing',
+          },
+        ],
+        htmlContent: 'Hello, this is testing',
+        subject: 'Testing this part',
+      },
+      {
+        headers: {
+          accept: 'application/json',
+          'api-key': process.env.BREVO_API_KEY || '',
+        },
+      }
+    )
+
+    // Handle the response here if needed
+    return response
+  } catch (error) {
+    // Handle errors here
+    console.error(error)
+  }
 }
