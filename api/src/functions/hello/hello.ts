fd3b725cbb5da5a57e10a82474c123dbcbafb75f
Jemuel Lupo <jemuel.lupo@gmail.com>
2023-10-26 23:04:38 +0800

    test(): asynchronously

--- api/src/functions/hello/hello.ts
+++ api/src/functions/hello/hello.ts
@@ -24,8 +24,8 @@ export const handler = async (_event: APIGatewayEvent, _context: Context) => {
 
   await Promise.all(
     [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20].map(
-      (i) => {
-        inngest.send('app/order.runner', {
+      async (i) => {
+        await inngest.send('app/order.runner', {
           data: {
             user_account_id: `Event-${i}`,
           },
